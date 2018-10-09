46db4ec42ab1e2596461325514f1e8dbda42d0e2
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-10-09 11:54:59 +0100

    Flux demo

--- mod-04/src/demo.js
+++ mod-04/src/demo.js
@@ -1,1 +1,46 @@
-console.log('aaannsns')
+function createAddAction(value) {
+  const action = {
+    type: "add",
+    payload: value,
+    error: null
+  };
+
+  return action;
+}
+
+const listeners = [];
+
+const store = {
+  state: 0,
+  listen(action) {
+    switch (action.type) {
+      case "add":
+        this.state += action.payload;
+        break;
+    }
+
+    listeners.forEach(element => {
+      element(this.state);
+    });
+  }
+};
+
+function dispatch(action) {
+  store.listen(action);
+}
+
+const addAction = createAddAction(7);
+
+listeners.push(console.log)
+
+dispatch(addAction);
+
+listeners.push(state => console.log(`The state is: ${state}`))
+
+dispatch(createAddAction(42));
+
+dispatch(createAddAction(100));
+
+listeners.pop()
+
+dispatch(createAddAction(100));
